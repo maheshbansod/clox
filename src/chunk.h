056d7c8b3ab743ba1ace2832ac73ef6c55bd22db
Mahesh Bansod <coolkevin011@gmail.com>
2023-12-31 01:19:19 +0530

    add some binary operations

--- src/chunk.h
+++ src/chunk.h
@@ -8,6 +8,11 @@
 
 typedef enum {
   OP_CONSTANT,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NEGATE,
   OP_RETURN,
 } OpCode;
 
---
+++ src/vm.h
@@ -0,0 +1,31 @@
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+#include "value.h"
+#include <stdint.h>
+
+#define STACK_MAX 256
+
+typedef struct {
+  Chunk *chunk;
+  uint8_t *ip;
+  Value stack[STACK_MAX];
+  Value *stackTop;
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPERT_RUNTIME_ERROR,
+  INTERPRET_COMPILE_ERROR
+} InterpretResult;
+
+void initVM();
+void freeVM();
+
+InterpretResult interpret(Chunk *chunk);
+
+void push(Value value);
+Value pop();
+
+#endif
