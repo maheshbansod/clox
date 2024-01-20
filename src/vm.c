994f0ad6f3293d50b681d8cf58790b5138e65c7b
Mahesh Bansod <coolkevin011@gmail.com>
2024-01-20 21:15:41 +0530

    while loop

--- src/vm.c
+++ src/vm.c
@@ -212,6 +212,11 @@ static InterpretResult run() {
         vm.ip += offset;
       break;
     }
+    case OP_LOOP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
     case OP_RETURN:
       // printValue(pop());
       // printf("\n");
