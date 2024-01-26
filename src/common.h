107f7d5c45cdbb3dc8775dbda89b2df26222be19
Mahesh Bansod <coolkevin011@gmail.com>
2024-01-26 19:17:58 +0530

    add a flag to control all debug flags

--- src/common.h
+++ src/common.h
@@ -5,11 +5,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef ENABLE_DEBUG_CLOX
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 #define DEBUG_STRESS_GC
 #define DEBUG_LOG_GC
+#endif
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
